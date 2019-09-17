a847594d87a3f3c2544c4efe35f91291f154ee8e
Eliud niebles <eliudniebles@gmail.com>
2019-09-17 18:00:43 -0500

    se crear en grups grupos, seciones, seguimiente

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from users import models as users
+from locality import models as geo
 # Create your models here.
+jornadas = ['AM','PM']
+class grupos (models.Model):
+
+    #
+    name        = models.CharField(max_length=20)
+    matriculado = models.IntegerField()
+    jornadas    = models.CharField(max_length=2, choices=jornadas)
+    sede        = models.ForeignKey(geo.sede, on_delete=models.PROTECT)
+    user        = models.ForeignKey(users.data_users, on_delete=models.PROTECT) 
+
+class sesiones(models.Model):
+
+    pass
+
+class seguimiento(models.Model):
+
+    pass
+    
